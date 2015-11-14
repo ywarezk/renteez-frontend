06b89feb8d49a441819356a2ac6137b77d07afa7
ywarezk <ywarezk@gmail.com>
2015-11-14 15:55:43 +0200

    seems like the serve static is now working

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
-var config = require('./webpack.config')
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var config = require('./webpack.config');
+var express = require('express');
 
-var app = new (require('express'))()
-var port = 3000
+var app = new (require('express'))();
+var port = 3000;
 
-var compiler = webpack(config)
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
-app.use(webpackHotMiddleware(compiler))
+var compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+app.use(webpackHotMiddleware(compiler));
 
-app.use(function(req, res) {
-  res.sendFile(__dirname + '/index.html')
-})
+app.get('*',function(req, res, next) {
+  if(req.url.substr(0,8) === '/public/'){
+    return next();
+  }
+  else{
+    res.sendFile(__dirname + '/index.html');
+  }
+});
+app.use("/public", express.static(__dirname + '/public/'));
 
 app.listen(port, function(error) {
   if (error) {
     console.error(error)
   } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
   }
-})
+});
